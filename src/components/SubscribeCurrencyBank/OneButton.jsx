81b8676e657b165d67107a7a545c7e4261a1d6bd
miron <miron.ivasch@gmail.com>
2020-05-16 14:09:35 +0300

    Made asynchrony for Subscription(Promise)

--- src/components/SubscribeCurrencyBank/OneButton.jsx
+++ src/components/SubscribeCurrencyBank/OneButton.jsx
@@ -5,54 +5,29 @@ import { connect } from 'react-redux';
 import { createPost } from '../../store/actions/postListActions';
 import './subscribeCurrencyBank.css';
 
-// const translationsText = {
-//     textSubscribe: {
-//         en: 'Subscribe',
-//         ru: 'Подписаться'
-//     },
-//     textUnsubscribe: {
-//         en: 'Unsubscribe',
-//         ru: 'Отписаться'
-//     }
-// }
-
 
 class OneButton extends React.Component {
-
-
     state = {
-        //  subscribe: 'sub',
         subscribe: this.props.currency.subscribe,
-
         //Для подписки
         currency_id: this.props.currency.currency_id
     }
 
     subscribeHandleClick = () => {
 
-        this.setState({
-
-            subscribe: this.state.subscribe === 'sub' ? 'unsub' : 'sub',
-        })
-
-        // if (this.state.subscribe === 'sub') {
-
-        //     this.setState({
-        //         currency_id: this.props.currency.currency_id,
-        //     },
-        //         () => console.log(this.props.currency.subscribe, 'MIRON')
-        //     )
-        // }
-        // else if (this.state.subscribe === 'unsub') {
-
-        //     this.setState({
-        //         currency_id: this.props.currency.currency_id,
-        //     },
-        //         () => console.log(this.props.currency.subscribe, 'MIRON'))
-        // }
-
-        //вызываю эту функцию с actions для подписки (POST)
-        this.props.createPost(this.state.currency_id, this.state.subscribe)
+        const changeState = () => {
+            return new Promise(((resolve, reject) => {
+                this.setState({
+                    subscribe: this.state.subscribe === 'sub' ? 'unsub' : 'sub',
+                })
+                resolve(this.state.subscribe)
+            }))
+        }
+        changeState().then((res) => {
+            console.table(res)
+            //вызываю эту функцию с actions для подписки (POST)
+            this.props.createPost(this.state.currency_id, this.state.subscribe)
+        });
 
     }
 
@@ -61,7 +36,7 @@ class OneButton extends React.Component {
     render() {
         let buttonColor = this.state.subscribe === 'sub' ? 'danger' : 'success';
 
-        
+
         const translations = {
             currencyText: {
                 en: this.props.currency.currency_name_en,
@@ -82,10 +57,8 @@ class OneButton extends React.Component {
         return (
             <>
                 <LanguageContext.Consumer>
-
                     {/* ${translations.textSubscribe[value]} */}
                     {(value) => <Button className="button_sub" onClick={this.subscribeHandleClick} variant={buttonColor}>{`${translations.currencyText[value]} ${textSubscribe} `}</Button>}
-
                 </LanguageContext.Consumer>
 
             </>
