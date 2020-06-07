4105e17e17112c6cac5aa3a56432f3a635928d31
miron <miron.ivasch@gmail.com>
2020-06-07 19:18:22 +0300

    Icons

--- src/components/NavbarFooter/Footer.jsx
+++ src/components/NavbarFooter/Footer.jsx
@@ -6,10 +6,11 @@ function Footer(props) {
         <div className='test'>
             <footer className="footer">
                 <div className="flexFooter">
-                    <a href="https://github.com/Miron2000" target="_blank"><span className="icon-github"></span></a>
-                    <a href="https://www.facebook.com/miron.ivashchenko" target="_blank"><span className="icon-facebook"></span></a>
-                    <a href="https://t.me/currency_ua" target="_blank"><span className="icon-telegram"></span></a>
-                    <a href="https://miron.ivasch@gmail.com" target="_blank"><span className="icon-google"></span></a>
+                    <a href="https://github.com/Miron2000" target="_blank"><i class="fa fa-github" className="icon-github" aria-hidden="true"></i></a>
+                    <a href="https://www.facebook.com/miron.ivashchenko" target="_blank"><i class="fa fa-facebook" className="icon-facebook" aria-hidden="true"></i></a>
+                    <a href="https://t.me/currency_ua" target="_blank"><i class="fa fa-telegram" className="icon-telegram" aria-hidden="true"></i></a>
+                    <a href="https://miron.ivasch@gmail.com" target="_blank"><i class="fa fa-google" className="icon-google" aria-hidden="true"></i></a>
+                   
                 </div>
             </footer>
         </div>
