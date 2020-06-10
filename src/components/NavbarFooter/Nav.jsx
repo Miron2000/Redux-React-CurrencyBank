a2b48d715b9b93a74e36fde0e0a1029ea0f9353a
miron <miron.ivasch@gmail.com>
2020-06-10 12:28:31 +0300

    Logo

--- src/components/NavbarFooter/Nav.jsx
+++ src/components/NavbarFooter/Nav.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Navbar, Nav as Navigation } from 'react-bootstrap';
 import './NavFooter.css';
+import logo from "../../img/logo.png";
 
 function Nav(props) {
     return (
         <div className='miron'>
             <Navbar bg="dark" variant="dark" expand="lg" className="Nav" sticky='top'>
                 <Navigation className="mr-auto">
-                    <Navbar.Brand className="text_logo">Currency</Navbar.Brand>
+                    <Navbar.Brand className="text_logo"><img className="imgLogoNav" src={logo}/></Navbar.Brand>
                     <div className="text_children">
                         {/* Children в App.js */}
                         <span className="childrenNav">{props.children}</span>
